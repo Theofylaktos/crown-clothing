8bde862b7c19ce47996f8ed79777ef2a98e13f95
theofylaktos <theofylaktos.a@gmail.com>
2020-02-28 03:28:51 +0200

    added header navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom'
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 import './App.css';
 
@@ -15,10 +16,11 @@ const HatsPage = () => (
 function App() {
   return (
       <div>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-        </Switch>
+          <Header/>
+          <Switch>
+              <Route exact path='/' component={HomePage} />
+              <Route exact path='/shop' component={ShopPage} />
+          </  Switch>
       </div>
   );
 }
